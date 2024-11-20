bf680fec9f99fe3f9747857336feced18b4cb000
Filippo <pippo.faccini@gmail.com>
2024-11-20 11:29:53 +0100

    feat: add RAII file

---
+++ core/src/utils/raii_file.hpp
@@ -0,0 +1,12 @@
+#include <cstdio>
+#include <memory>
+
+struct FileCloser {
+  void operator()(FILE *file) const {
+    if (file) {
+      fclose(file);
+    }
+  }
+};
+
+typedef std::unique_ptr<FILE, FileCloser> RAIIFile;
